4b44f02e540d13c67e5f654f21d6b4c2d23a4f0f
xiaoDao776 <3884075394@qq.com>
2026-08-08 08:58:11 +0800

    Delete src-theme directory

--- src-theme/src/routes/hud/elements/Watermark.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import LiquidBounceLogo from "../../../components/LiquidBounceLogo.svelte";
-</script>
-
-<LiquidBounceLogo
-    width="165px"
-    height="61.8917px"
-    badgeFill="var(--accent-color)"
-/>
